47c9563d8eabfe757b7cb9c281516ea8277248df
Adrian Fulladolsa <ful21592@uvg.edu.gt>
2024-08-01 22:07:09 -0600

    Go SSL with Vue Setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/adrianfulla/Proyecto1-Redes
+
+go 1.22.1
---
+++ server/server.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// http.HandleFunc("/", homePageHandler)
+
+	http.HandleFunc("/api/thumbnail", thumbnailHandler)
+
+	fs := http.FileServer(http.Dir("../frontend/dist"))
+
+	http.Handle("/", fs)
+
+	fmt.Println("Server listening on port 3000")
+	log.Panic(
+		http.ListenAndServe(":3000", nil),
+	)
+}
+
+func homePageHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintf(w, "hello world")
+	checkError(err)
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+type thumbnailRequest struct {
+	Url string `json:"url"`
+}
+
+func thumbnailHandler(w http.ResponseWriter, r *http.Request) {
+	var decoded thumbnailRequest
+
+	err := json.NewDecoder(r.Body).Decode(&decoded)
+	if err != nil {
+		fmt.Printf("Got a thumbnail")
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Printf("Got the following url: %s\n", decoded.Url)
+}
