56d15880a5695e6f4593631c76e05ba0235d807e
Adrian Fulladolsa <ful21592@uvg.edu.gt>
2024-08-13 12:16:06 -0600

    Connection for Authentication

--- server/xmpp/conn.go
+++ server/xmpp/conn.go
@@ -3,43 +3,35 @@ package xmpp
 import (
 	"crypto/tls"
 	"net"
-	"time"
-	"fmt"
 )
 
 type XMPPConnection struct {
     Conn net.Conn
 }
 
-func NewXMPPConnection(address string, useTLS bool) (*XMPPConnection, error) {
-    var conn net.Conn
-    var err error
+func (c *XMPPConnection) Write(data []byte) (int, error) {
+	return c.Conn.Write(data)
+}
 
-	dialer := &net.Dialer{
-        Timeout: 5 * time.Second,
-    }
+func (c *XMPPConnection) Read(buffer []byte) (int, error) {
+	return c.Conn.Read(buffer)
+}
 
-    if useTLS {
-		// TLS config with InsecureSkipVerify to bypass certificate verification
-        tlsConfig := &tls.Config{
-            InsecureSkipVerify: true, // Disable certificate verification
-            ServerName:         "alumchat.lol",
-        }
+func NewXMPPConnection(server string, useTLS bool) (*XMPPConnection, error) {
+	var conn net.Conn
+	var err error
 
-        // Directly establish a TLS connection, effectively skipping STARTTLS
-        conn, err = tls.DialWithDialer(dialer, "tcp", address, tlsConfig)
-        if err != nil {
-            return nil, fmt.Errorf("TLS connection failed: %w", err)
-        }
-    } else {
-        conn, err = net.Dial("tcp", address)
-    }
+	if useTLS {
+		conn, err = tls.Dial("tcp", server, &tls.Config{InsecureSkipVerify: true})
+	} else {
+		conn, err = net.Dial("tcp", server)
+	}
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    return &XMPPConnection{Conn: conn}, nil
+	return &XMPPConnection{Conn: conn}, nil
 }
 
 func (xc *XMPPConnection) Close() error {
